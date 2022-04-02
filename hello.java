43d9057881e827f2840ed55be5d3c02cf2505761
prabhat <prabhatmishra899@gmail.com>
2022-04-02 15:00:31 +0530

    String practise in java

---
+++ hello.java
@@ -0,0 +1,26 @@
+import javax.print.DocFlavor.STRING;
+
+class first 
+{
+    public static void main(String args[])
+    {
+        // string 
+
+        // concatenate
+        String fname = "Prabhat";
+        String lname = "Mishra";
+
+        System.out.println(fname + lname);
+
+        // length  { it's function will count the length with white space}
+        System.out.println(fname.length());
+
+        // charAt() : variable_name.charAt(indexno.) {here index no. starting with 0}
+        System.out.println(fname.charAt(0));
+
+
+
+         
+
+    }
+} 

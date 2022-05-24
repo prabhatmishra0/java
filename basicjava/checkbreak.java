acf1f40638c3ad48cce8c824b7e63916be26ef84
Prabhat <prabhatmishra899@gmail.com>
2022-05-24 23:48:44 +0530

    check loop

---
+++ basicjava/checkbreak.java
@@ -0,0 +1,12 @@
+public class checkbreak{
+    public static void main(String[] args){
+
+            for(int i=0; i<9; i++){
+                if(i==3){
+                    break;
+                }
+                System.out.println(i);
+                
+            }
+    }
+}

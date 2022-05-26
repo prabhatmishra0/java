4416d4cbcc1aec4b044005babce552de21ecdb38
Prabhat <prabhatmishra899@gmail.com>
2022-05-27 00:28:20 +0530

    Holo rombus and solid rombus complete

--- Solid_Rohmbus.java
+++ Solid_Rohmbus.java
@@ -1,9 +1,36 @@
 public class Solid_Rohmbus {
+
+
+    public static void main(String[] args){
+
+    // Author : Prabhat Mishra
+    // print the solid rohmbus
+
+    int m = 5;
+
+    for(int i = 1; i<=m; i++){
+
+        // print space 
+        
+        for(int j = 1; j<=m-i; j++){
+            System.out.print(" ");
+        }
+
+        // print star 
+
+        for(int j = 1; j <= m; j++){
+            System.out.print("*");
+        }
+
+        System.out.println();
+    }
+
+}
     
 }
 
 
 //     *****
-//    *****
-//   *****
-// *****    
+//    *   *
+//   *  *
+// **    
